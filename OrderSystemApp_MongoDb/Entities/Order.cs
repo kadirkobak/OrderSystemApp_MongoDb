4d158d4e27b0b64023f78ee660e6ef082cf4af1c
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-30 09:19:36 +0300

    update

---
+++ OrderSystemApp_MongoDb/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystemApp_MongoDb.Entities
+{
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public string CustomerName { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }   
+        public decimal TotalPrice { get; set; }
+    }
+}
