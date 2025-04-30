97c3bdbac02b09b6fce437144d57e5409787ef7e
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-30 10:02:20 +0300

    update

--- OrderSystemApp_MongoDb/Services/MongoDbConnection.cs
+++ OrderSystemApp_MongoDb/Services/MongoDbConnection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace OrderSystemApp_MongoDb.Services
@@ -14,6 +15,12 @@ namespace OrderSystemApp_MongoDb.Services
         public MongoDbConnection()
         {
             var client = new MongoClient("mongodb://localhost:27017");
+            _database = client.GetDatabase("OrderSystemApp");
+        }
+
+        public IMongoCollection<BsonDocument> GetOrdersCollection()
+        {
+            return _database.GetCollection<BsonDocument>("Orders");
         }
     }
 }
