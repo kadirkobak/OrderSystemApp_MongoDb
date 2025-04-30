ed1c1d634b9c5f27cb9de5798cf2b7b20bba309a
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-30 10:33:45 +0300

    update

---
+++ OrderSystemApp_MongoDb/Services/OrderOperation.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using OrderSystemApp_MongoDb.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystemApp_MongoDb.Services
+{
+    public class OrderOperation
+    {
+        public void AddOrder(Order order)
+        {
+            var connection = new MongoDbConnection();
+            var orderCollection = connection.GetOrdersCollection();
+
+            var document = new BsonDocument
+            {
+                { "OrderId", order.OrderId },
+                { "CustomerName", order.CustomerName },
+                { "District", order.District },
+                { "City", order.City },
+                { "TotalPrice", order.TotalPrice }
+            };
+        }
+    }
+}
